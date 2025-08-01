8686eaf934cb25387c927c1d02897b283c652492
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2025-08-01 16:25:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# main.py
+from fastapi import FastAPI
+from fetcher import get_today_summary
 
+app = FastAPI()
+
+@app.get("/summary/today")
+def today_summary():
+    return {"summary": get_today_summary()}
